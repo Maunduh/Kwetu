bf78f60c26ed2d64974e7f6d64aeff18be509bdc
amosmaundundu <amosmaundu5@gmail.com>
2023-05-08 16:57:44 +0300

    second commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,11 @@ import "./navbar.css"
 const Navbar = () => {
   return (
     <div className='navbar'>
+      <div className='Title'>
+      <h1>Kwetu Delicacies</h1>
+    </div>
       <div className='links'>
+
         <Link to='/'>Home</Link>
         <Link to='/cart'>
             <ShoppingCart size={32} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,9 +5,7 @@ import "./home.css"
 const Home = () => {
   return (
     <div className='home'>
-    <div className='Title'>
-      <h1>Kwetu Delicacies</h1>
-    </div>
+    
     <div className='meals'>{meals.map((meal)=>{ 
      return <Meal data={meal} />
     })}
